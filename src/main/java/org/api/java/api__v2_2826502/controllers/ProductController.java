e6744b9fedefae2311a41ddfe1f03e19299e57f6
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-08-06 17:39:58 -0500

    Evidencia 2

--- src/main/java/org/api/java/api__v2_2826502/controllers/ProductController.java
+++ src/main/java/org/api/java/api__v2_2826502/controllers/ProductController.java
@@ -3,16 +3,12 @@ package org.api.java.api__v2_2826502.controllers;
 import org.api.java.api__v2_2826502.entities.Product;
 import org.api.java.api__v2_2826502.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
-
+@RequestMapping("/api")
 public class ProductController {
 
     //inyectar el servicio
@@ -22,14 +18,41 @@ public class ProductController {
     //endpoint
     //listar todos los productos
     @GetMapping
-    public List<Product> getProducts(){
-    return productService.findAll();
+    public List<Product> getProducts() {
+        return productService.findAll();
 
     }
+
     //End Point
     //obtener un producto por id
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable Long id){
+    public Product getProduct(@PathVariable Long id) {
         return productService.findById(id);
     }
+
+
+    //Endpoint
+    //crear un producto
+
+    @PostMapping("/products")
+    public Product crearProducto
+            (@RequestBody Product product) {
+        return productService.save(product);
+    }
+    //Endpoint: Actualizar producto
+    @PutMapping("/products/{id}")
+    public Product actualizarProducto(
+            @RequestBody Product product,
+            @PathVariable Long id){
+       return productService.cambiarProduct(product, id);
+
+    }
+   @DeleteMapping("/products/{id}")
+   public Product borrarProducto(
+           @PathVariable Long id){
+        return productService.eliminarProducto( id);}
+
+
+
 }
+
--- src/main/java/org/api/java/api__v2_2826502/services/ProductService.java
+++ src/main/java/org/api/java/api__v2_2826502/services/ProductService.java
@@ -1,15 +1,20 @@
 package org.api.java.api__v2_2826502.services;
 
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.Setter;
 import org.api.java.api__v2_2826502.entities.Product;
 import org.api.java.api__v2_2826502.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Service
+
 public class ProductService {
     //Dependencia inyectada
     //objeto necesario para cumplir
@@ -26,11 +31,46 @@ public class ProductService {
 
 
     }
-//seleccionar producto por id
+    //seleccionar producto por id
     //SQl : selec: from whre id=
 
     public Product findById(Long id){
         return repositorio.findById(id).get();
+
     }
 
+
+    //insetar nuevo producto
+    @Transactional
+    public Product save(Product product){
+        //save:guardar nuevo producto
+        //      en bd
+        return repositorio.save(product);
+    }
+
+
+    //actualizar productos
+    @Transactional
+    public Product cambiarProduct(
+            Product product, long id){
+        //1.obtener el producto por id
+        Product prodUpd = this.findById(id);
+        //2. actualizar el estado del objeto
+        // encontrado
+        prodUpd.setDescription(product.getDescription());
+        prodUpd.setPrice(product.getPrice());
+        prodUpd.setName(product.getName());
+        //3.grabar(guardar) cambiar bd
+        return repositorio.save(prodUpd);
+    }
+    @Transactional
+    public Product eliminarProducto(
+             long id){
+        Product proDlt = this.findById(id);
+        repositorio.delete(proDlt);
+        return proDlt;
+    }
+
+
 }
+
